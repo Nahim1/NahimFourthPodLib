a05fb4788bb58feaf54a8b7c1fd4b8531fb7f9b8
Nahim1 <nahim.makhani7786@gmail.com>
2023-11-29 11:06:12 +0530

    initial commit

--- Example/NahimFourthPodLib/ViewController.swift
+++ Example/NahimFourthPodLib/ViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
-
+import NahimFourthPodLib
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func btnClick(_ sender: Any) {
+        let myBundle = Bundle(for: AuthViewController.self)
+        let auth = AuthViewController(nibName: "AuthViewController",bundle: myBundle)
+        self.navigationController?.present(auth, animated: true)
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ NahimFourthPodLib/Classes/AuthViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AuthViewController.swift
+//  NahimFourthPodLib
+//
+//  Created by APPLE on 29/11/23.
+//
+
+import UIKit
+
+public class AuthViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func btnDismiss(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
